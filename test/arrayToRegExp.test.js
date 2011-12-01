786ae23bc9902cf11f9d3ab282a4e496c795fb15
Edward Hotchkiss <e@ingk.com>
2011-12-01 07:09:09 -0800

    tests for arrayToRegExp()

---
+++ test/arrayToRegExp.test.js
@@ -0,0 +1,18 @@
+
+var vows = require('vows');
+var assert = require('assert');
+
+var belt = new require('../lib/belt');
+
+vows.describe('belt.arrayToRegExp() tests').addBatch({
+  'when creating an array and using belt.arrayToRegExp':{
+    topic:function(){
+      return belt.arrayToRegExp(['these','are','test','items']);
+    },
+    'typeOf should be a regexp':function(topic){
+      assert.equal(belt.typeOf(topic), 'regexp');
+    }
+  }
+}).export(module);
+
+/* EOF */
