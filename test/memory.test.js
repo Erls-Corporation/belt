efba1e868f5e54258aced30aa2115c018f9c0e6d
Edward Hotchkiss <e@ingk.com>
2011-12-01 07:24:02 -0800

    memory module integrated into belt.memory()

---
+++ test/memory.test.js
@@ -0,0 +1,19 @@
+
+var vows = require('vows');
+var assert = require('assert');
+
+var belt = new require('../lib/belt');
+
+vows.describe('belt.memory() tests').addBatch({
+  'when calculating current memory usage':{
+    topic:function(){
+      return belt.memory(4, true);
+    },
+    'typeOf should be a string and not be null':function(topic){
+      assert.notEqual(topic, null);
+      assert.equal(belt.typeOf(topic), 'string');
+    }
+  }
+}).export(module);
+
+/* EOF */
